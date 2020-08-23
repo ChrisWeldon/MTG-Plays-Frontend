e8416863c90eb59437d0226c01b37ab0becf73fd
cwevans612@gmail.com <chrisevans@Chriss-MacBook-Air.local>
2020-08-23 16:30:31 -0400

    Added build

---
+++ build/precache-manifest.88688863bbd413b1f226ea473bac424f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14dea04c81b1a607f46c2c47c4368834",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fdeff4f40906671e5595",
+    "url": "/static/css/main.bfa1645d.chunk.css"
+  },
+  {
+    "revision": "86f1edf76bdc26ae1ff2",
+    "url": "/static/js/2.9b40408a.chunk.js"
+  },
+  {
+    "revision": "c7a7e79b10ab645cab64be742e1dd9e5",
+    "url": "/static/js/2.9b40408a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdeff4f40906671e5595",
+    "url": "/static/js/main.8b70c68a.chunk.js"
+  },
+  {
+    "revision": "f721334fab336bd81d07",
+    "url": "/static/js/runtime-main.9f055bbf.js"
+  }
+]);
